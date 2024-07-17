768f68b4c9978408d072c7896ad54206be6a492f
puja choursiya <choursiyapuja21@gmail.com>
2024-07-17 12:46:55 +0530

    corousel change

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,7 +1,5 @@
 import React from "react";
-//import shapeImg from "../img/img-wave2-with-centered-text.png";
 import { Link } from "react-router-dom";
-
 
 const Carousel = () => {
   return (
@@ -11,106 +9,26 @@ const Carousel = () => {
           <source src={require('../img/upstartai-video.mp4')} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
-        {/*<img className="shape" src={shapeImg} alt="#" />*/}
         <div className="container">
           <div
-            className="row align-items-center"
+            className="row align-items-center content"
             data-aos="fade-up"
             data-aos-delay="200"
           >
             <div className="col-lg-7 col-md-12 col-12">
-             {/* <h2 data-aos="fade-up">
-                Harness Data To Empower Your Business
-              </h2>
-              <br></br>
-              <br></br>
-              */}
               <blockquote data-aos="fade-up" data-aos-delay="100">
-                <p>
-                  UpstartAI: One-Stop Solution <br></br>for All Your Digital Needs.
+                <p className="blockquote-text">
+                  UpstartAI: One-Stop Solution <br />for All Your Digital Needs.
                 </p>
-              </blockquote>
-              <div className="d-flex align-items-center">
-                <Link to="/about" className="btn-get-started">
-                  Know More
-                </Link>
-              </div>
-            </div>
-
-            
-            {/* Hide or remove this part to hide carousel images 
-            <div className="col-lg-5 col-md-12 col-12">
-              <div className="header-image d-none">
-                <div
-                  id="carouselExampleFade"
-                  className="carousel slide carousel-fade"
-                  data-bs-ride="carousel"
-                >
-                  <div className="carousel-indicators">
-                    <button
-                      type="button"
-                      data-bs-target="#carouselExampleFade"
-                      data-bs-slide-to="0"
-                      className="active"
-                      aria-current="true"
-                      aria-label="Slide 1"
-                    ></button>
-                    <button
-                      type="button"
-                      data-bs-target="#carouselExampleFade"
-                      data-bs-slide-to="1"
-                      aria-label="Slide 2"
-                    ></button>
-                    <button
-                      type="button"
-                      data-bs-target="#carouselExampleFade"
-                      data-bs-slide-to="2"
-                      aria-label="Slide 3"
-                    ></button>
-                    <button
-                      type="button"
-                      data-bs-target="#carouselExampleFade"
-                      data-bs-slide-to="3"
-                      aria-label="Slide 4"
-                    ></button>
-                  </div>
-                  <div className="carousel-inner">
-                    <div className="carousel-item active">
-                      <img
-                        src="#"
-                        className="d-block w-100"
-                        alt="..."
-                      />
-                    </div>
-                    <div className="carousel-item">
-                      <img
-                        src="#"
-                        className="d-block w-100"
-                        alt="..."
-                      />
-                    </div>
-                    <div className="carousel-item">
-                      <img
-                        src="#"
-                        className="d-block w-100"
-                        alt="..."
-                      />
-                    </div>
-                    <div className="carousel-item">
-                      <img
-                        src="#"
-                        className="d-block w-100"
-                        alt="..."
-                      />
-                    </div>
-                  </div>
+                <div className="d-flex align-items-center">
+                  <Link to="/about" className="btn-get-started blockquote-link">
+                    Know More
+                  </Link>
                 </div>
-              </div>
+              </blockquote>
             </div>
-             End of hidden carousel part */}
           </div>
         </div>
-        
       </section>
     </>
   );
